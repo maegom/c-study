979d5149291ab5bbf9b19686137d4f5a5b8e6862
SeungHo Hwang <hoho7013@gmail.com>
2023-07-31 10:58:28 +0900

    0731 class 이론

---
+++ 0731/0731/main1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class CGameManager
+{
+public:
+	CGameManager();
+	~CGameManager();
+
+private:
+
+public:
+	void init()
+	{
+
+	}
+	void Run()
+	{
+
+	}
+
+};
+
+CGameManager::CGameManager()
+{
+}
+
+CGameManager::~CGameManager()
+{
+}
+
+int main()
+{
+	CGameManager Mgr;
+	return 0;
+}
